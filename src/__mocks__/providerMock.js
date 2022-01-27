a24c679883b881ca219a6389e4ae8ed42212f3e2
Carlos Andres Gonzalez <cgonzalez@fullstacklabs.co>
2022-01-26 19:17:16 -0500

    Update for ProviderMock / Header tests

--- src/__mocks__/providerMock.js
+++ src/__mocks__/providerMock.js
@@ -3,18 +3,19 @@ import { createStore } from 'redux';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createBrowserHistory } from 'history';
-import reducer from './reducers';
-import initialState from './initialState';
+import reducer from '../reducers';
+import initialState from '../initialState';
 
-const store = createStore(reducer, initialState);
 const history = createBrowserHistory();
 
-const ProviderMock = props => (
-  <Provider store={store}>
-    <Router history={history}>
-      {props.children}
-    </Router>
-  </Provider>
-);
+const ProviderMock = props => {
+  const { customState } = props;
+  const store = createStore(reducer, {...initialState, ...customState});
+  return (
+    <Provider store={store}>
+      <Router history={history}>{props.children}</Router>
+    </Provider>
+  );
+}
 
 export default ProviderMock;
